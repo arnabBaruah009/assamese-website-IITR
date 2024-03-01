92a4a50d6243f7eac4ba122186503c9ddff8d185
Arnab Baruah <baruaharnab769@gmail.com>
2024-03-01 07:53:49 +0530

    Created routes for the pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, Members, Gallery } from "../pages";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
