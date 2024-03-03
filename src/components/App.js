fe1861c56cae2652795e423700e70cec831a6833
Arnab Baruah <baruaharnab769@gmail.com>
2024-03-04 03:46:30 +0530

    Added navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Members, Gallery } from "../pages";
+import { Navbar } from "./";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/members" element={<Members />} />
           <Route path="/gallery" element={<Gallery />} />
+          <Route path="/members" element={<Members />} />
         </Routes>
       </div>
     </BrowserRouter>
